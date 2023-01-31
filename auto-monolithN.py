ada7548484991449dbb6f7ab031793e9cf3cac2a
DiegoLorenzo7 <78904840+DiegoLorenzo7@users.noreply.github.com>
2023-01-31 14:30:26 +0100

    Add files via upload

---
+++ auto-monolithN.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+
+#PRACTICA CREATIVA 2
+
+
+#CDPS
+
+
+
+
+
+import sys
+from subprocess import call 
+import os
+
+#Guardamos la variable del entorno
+
+num_grupo= str(os.environ.get ("GROUP_NUMBER"))
+
+# Descargamos el repositorio 
+
+call (["git", "clone", "https://github.com/DiegoLorenzo7/practica-creativa2"])
+
+#instalamos pip
+call(["sudo", "apt-get", "install", "python3-pip"])
+
+
+call(["sudo", "apt-get", "update"])
+# Instalamos dependencias 
+
+call (["pip3", "install", "-r" , "practica-creativa2/bookinfo/src/productpage/requirements.txt"])
+
+
+#Cambiamos titulo 
+call (["cp", "practica-creativa2/bookinfo/src/productpage/templates/productpage.html",
+"practica-creativa2/bookinfo/src/productpage/templates/productpageCopy.html"])
+fCopia = open ("practica-creativa2/bookinfo/src/productpage/templates/productpageCopy.html", 'r')
+fOriginal = open ("practica-creativa2/bookinfo/src/productpage/templates/productpageCopy.html", 'w')
+for line in fCopia:
+        if "{% block title %} Simple Bookstore App{& endblock %}" in line:
+             fOriginal.write("{% block title %} Simple Bookstore App - GRUPO " + num_grupo + "{% endblock %}") 
+        else: fOriginal.write(line)
+fOriginal.close()
+fCopia.close()
+
+
+
+# Arrancamos aplicacion
+
+call (["python3", "practica-creativa2/bookinfo/src/productpage/productpage_monolith.py", "9080"])
