02a3f68e8235b6eaa82eef457e018e250d11d1ec
DiegoLorenzo7 <78904840+DiegoLorenzo7@users.noreply.github.com>
2023-01-31 19:38:13 +0100

    Add files via upload

---
+++ parte2/apliDocker.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+
+
+import sys
+from subprocess import call
+import os
+
+
+
+
+
+# GUARDAMOS EL VALOR DE LA VARIABLE DE ENTORNO
+numeroGrupo = os.environ.get("GROUP_NUMBER")
+
+# COMANDOS NECESARIOS PARA LA PUESTA EN MARCHA DE LA APLICACIÓN	
+call(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2.git"], shell=True)
+
+
+# ACTUALIZAMOS EL SISTEMA
+call(["sudo", "apt-get", "update"])
+
+
+# INSTALAMOS PIP
+call(["sudo", "apt-get", "install", "python3-pip"])
+
+
+# AÑADIMOS LOS REQUIREMENTS NECESARIOS
+call(["pip3", "install", "-r", "practica_creativa2/bookinfo/src/productpage/requirements.txt"], shell=True)
+
+
+
+# MODIFICAMOS EL TITULO DE LA APLICACION
+call(["cp", "practica_creativa2/bookinfo/src/productpage/templates/productpage.html", "practica_creativa2/bookinfo/src/productpage/templates/productpageCopia.html"])
+fCopia = open("practica_creativa2/bookinfo/src/productpage/templates/productpageCopia.html", 'r')
+fOriginal = open("practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'w')
+for line in fCopia:
+    if "{% block title %}Simple Bookstore App{% endblock %}" in line:
+        fOriginal.write("{% block title %}Simple Bookstore App - GRUPO " + numeroGrupo + "{% endblock %}")
+    else:
+        fOriginal.write(line)
+fOriginal.close()
+fCopia.close()
+
+
+
+# ARRANCAMOS LA APLICACIÓN
+call(["python3", "practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", "9080"], shell=True)
